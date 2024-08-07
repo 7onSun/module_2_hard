a643b09682b56da62ff1bea20f546a673bbdf91a
7onSun <7onsun@mail.ru>
2024-08-07 22:10:41 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,10 +1,10 @@
 n = int(input('Введите число от 3 до 20: '))
 if n in range(3, 21):
-    result = ()
+    result = ''
     for i in range(1, n):
         for j in range(i + 1, n):
             if n % (i + j) == 0:
-                result += (i, j)
+                result += f'{i}{j}'
                 continue
     print(result)
 
